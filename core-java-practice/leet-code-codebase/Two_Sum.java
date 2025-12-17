f5502259eb19a0e328ff441100fbfdcced79d409
Gulshan-001 <gulshanthakur1010@gmail.com>
2025-12-17 15:04:34 +0530

    [Gulshan] First Five Problems

---
+++ core-java-practice/leet-code-codebase/Two_Sum.java
@@ -0,0 +1,19 @@
+public class Two_Sum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1}; 
+    }
+    public static void main(String[] args) {
+        Two_Sum ts = new Two_Sum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = ts.twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
